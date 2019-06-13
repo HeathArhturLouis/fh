8ffc0f38098c9832da2f242ba54dbd6d7ec742d5
Arhtur-Louis Heath <a.heath.17@aberdeen.ac.uk>
2019-06-13 13:43:54 +0000

    basic link following

--- src/ffCursesClasses.py
+++ src/ffCursesClasses.py
@@ -1,9 +1,33 @@
 import urwid
 import urwid.raw_display
 import urwid.web_display
+import regex as re
+from urllib.parse import urlparse
+
+import os
+import subprocess
+
 
 blank = urwid.Divider()
 
+###################TOOLS####################
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+
+
+def uri_validator(x):
+	try:
+		result = urlparse(x)
+		return all([result.scheme, result.netloc, result.path])
+	except:
+		return False
+
+
+
+#############################################
 class ffCUI(urwid.WidgetWrap):
 	'''
 	Top most widget, contains all elements on screen
@@ -26,17 +50,19 @@ class ffIdButton(urwid.Button):
 	Clas for buttons in feed menu, id: the id of the button in the channel
 	presumably this is what user_data is but the documentation struck me as unclear
 	'''
-	def __init__(self, label, signal, user_data='', id=-1):
+	def __init__(self, label, signal, user_data='', uri='', id=-1):
 		self.id = id
+		self.uri = uri
 		super(ffIdButton, self).__init__(label, signal)
-		#change display widget from default thingie
+		#change display widget from default thing
 
 		self._w = urwid.AttrMap(urwid.SelectableIcon(
             label, 2), None, 'body')
-def generateEntrybox(ffEntry):
+
+
+def generateEntrybox(ffEntry, uribinding):
 	'''
-	generate and return a summary of the news article with relevant information and a button
-	to visit the website
+	Return entrybox of buttons
 	'''
 	newsbox_content = []
 	possibleFields = []
@@ -45,12 +71,20 @@ def generateEntrybox(ffEntry):
 	for field in ffEntry.data.keys():
 		#add description atribute
 		try:
-			newsbox_content += [blank, urwid.Padding(urwid.AttrWrap( urwid.Text(ffEntry.data[field], align='center', wrap='space'), 'fbbody0' ), align='center',width=('relative', 80) ),]
+			if(uri_validator(ffEntry.data[field])):
+				#,make button
+				newsbox_content += [blank, urwid.Padding(urwid.AttrWrap( ffIdButton(cleanhtml(ffEntry.data[field]), uribinding, id='field', uri=cleanhtml(ffEntry.data[field])), 'fbbody0' ), align='center',width=('relative', 80) ), ]
+			else:
+				newsbox_content += [blank, urwid.Padding(urwid.AttrWrap( urwid.Text(cleanhtml(ffEntry.data[field])), 'fbbody0' ), align='center',width=('relative', 80) ),]
 		except:
-			pass	
+			pass
 
-	newsbox_content += [blank, urwid.Padding(urwid.AttrWrap( urwid.Text(ffEntry.source, align='center', wrap='space'), 'fbbody0' ), align='center',width=('relative', 80) ), ]
-	return urwid.AttrWrap(urwid.ListBox(newsbox_content), 'fbbody0')
+	newsbox_content += [blank, urwid.Padding(urwid.AttrWrap( urwid.Text(cleanhtml(ffEntry.source)), 'fbbody0'), align='center',width=('relative', 80) ), ]
+#	newsbox_content += [blank, urwid.Padding(urwid.AttrWrap( urwid.Text(cleanhtml(ffEntry.source), lambda x: None , id='source'), 'fbbody0' ), align='center',width=('relative', 80) ), ]
+
+	walker = urwid.SimpleListWalker(newsbox_content)
+
+	return urwid.Frame(urwid.AttrWrap(urwid.ListBox(walker), 'fbbody0'))
 
 
 def generateFeedbox(ffEntries, binding, offset=False):
@@ -61,8 +95,12 @@ def generateFeedbox(ffEntries, binding, offset=False):
 	colors = ['fbbody0', 'fbbody1'] 
 	feedbox_content = []
 	for entry in ffEntries:
+			if('link' in entry.data):
+				link = entry.data.link
+			else:
+				link=None
 			#item = FeedItemWidget(i, entry.data.title, select_article, entry)
-			item = urwid.AttrMap(ffIdButton(entry.data.title, binding, id=i), colors[i%2])
+			item = urwid.AttrMap(ffIdButton(entry.data.title, binding, id=i, uri=entry.data.link), colors[i%2])
 			feedbox_content.append(item)
 			i+=1
 
@@ -74,12 +112,35 @@ class ffFeedViewer(urwid.WidgetWrap):
 	'''
 	Feed view and display entry data for a datafeed
 	'''
+
+	def visitAdress(self, button):
+#todo: config this command somewhare lol
+		#execute command:
+		#redicrect output to stop annoying echo
+		FNULL = open(os.devnull, 'w')
+		subprocess.run(["chromium", 'https://outline.com/' + button.uri], stdout=FNULL, stderr=FNULL)
+
 	def switchFocus(self, button):
-		self.entrybox = generateEntrybox(self.channel.entries[button.id])
+		self.entrybox = generateEntrybox(self.channel.entries[button.id], self.visitAdress)
 		self._w = urwid.Columns([('weight', 7, self.feedbox),('weight', 3, self.entrybox)])
 
+
+	def entrySelectPress(self, button):
+		#when an entry has been selected
+		if (self.lastSelected == button.id):
+			#virtual button with adress
+			self.visitAdress(button)
+		else:
+			self.switchFocus(button)
+			self.lastSelected = button.id
+
+
 	def __init__(self, ffChannel):
+		#keep track of last entry pressed for double click
+		self.lastSelected = -1
+
 		self.channel = ffChannel
-		self.feedbox = generateFeedbox(self.channel.entries, self.switchFocus)
-		self.entrybox = generateEntrybox(ffChannel.entries[0])
+		self.feedbox = generateFeedbox(self.channel.entries, self.entrySelectPress)
+		self.entrybox = generateEntrybox(ffChannel.entries[0], self.visitAdress)
 		super(ffFeedViewer, self).__init__(urwid.Columns([('weight', 7, self.feedbox),('weight', 3, self.entrybox)]))
+
